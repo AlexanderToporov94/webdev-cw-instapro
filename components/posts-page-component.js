a5ab60021b29f447b066fec35b5e0c34760f0987
AlexanderToporov94 <tav003@mail.ru>
2023-07-16 17:13:47 +0300

    add postsHTML

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -10,6 +10,34 @@ export function renderPostsPageComponent({ appEl }) {
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
+  const postsHTML = posts.map((user,index) => {
+    return`
+    <li class="post">
+      <div class="post-header" data-user-id=${user.id}>
+          <img src=${user.imageUrl} class="post-header__user-image">
+          <p class="post-header__user-name">${user.name}</p>
+      </div>
+      <div class="post-image-container">
+        <img class="post-image" src=${imageUrl}>
+      </div>
+      <div class="post-likes">
+        <button data-post-id="642d00579b190443860c2f32" class="like-button">
+          <img src="./assets/images/like-active.svg">
+        </button>
+        <p class="post-likes-text">
+          Нравится: <strong>2</strong>
+        </p>
+      </div>
+      <p class="post-text">
+        <span class="user-name">${user.name}</span>
+        ${description}
+      </p>
+      <p class="post-date">
+        ${createdAt}
+      </p>
+    </li>`
+  }).join('');
+
   const appHtml = `
               <div class="page-container">
                 <div class="header-container"></div>
